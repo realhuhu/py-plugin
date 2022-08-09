aaae5dab98bb479d442e227f50dd2b01c5b6e180
realhuhu <2661467107@qq.com>
2022-08-09 11:54:21 +0800

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { exec } from "child_process";
 import fs from "fs";
 import path from "path";
+import _ from "lodash";
 import { _path, config } from "./core/client/client.js";
 
 if (config.host === "127.0.0.1") {
@@ -15,10 +16,18 @@ let apps = [];
 for (let file of files) {
   let tmp = await import(`./apps/js/${file}`);
   if (tmp.rule) {
-    apps.push(tmp);
+    for (let key of Object.keys(tmp.rule)) {
+      apps.push({
+        reg: tmp.rule[key].reg,
+        describe: tmp.rule[key].describe,
+        priority: tmp.rule[key].priority,
+        handler: tmp[key],
+      });
+    }
   }
-}
 
+  apps = _.sortBy(apps, app => app.priority);
+}
 
 export const rule = {
   proxy: {
@@ -30,19 +39,19 @@ export const rule = {
 
 export async function proxy(e) {
   for (let app of apps) {
-    for (let key of Object.keys(app.rule)) {
-      if (new RegExp(app.rule[key].reg).test(e.msg) || app.rule[key].reg === "noCheck") {
-        try {
-          let stop = app[key](e);
-          if (app.rule[key].reg !== "noCheck") console.log(`py-plugin:${app[key].name}`);
-          if (stop) return true;
-        } catch (e) {
-          console.log(`py-plugin:${app[key].name} error:${e}`);
-        }
+    if (new RegExp(app.reg).test(e.msg) || app.reg === "noCheck") {
+      try {
+        let stop = await app.handler(e);
+        if (app.reg !== "noCheck") console.log(`py-plugin:${app.handler.name}`);
+        if (app.reg === "noCheck" && stop) console.log(`py-plugin:${app.handler.name}`);
+        if (stop) return true;
+      } catch (e) {
+        console.log(`py-plugin:${app[key].name} error:${e}`);
       }
     }
   }
 }
+
 
 export class Proxy {
   name = "py-plugin";
