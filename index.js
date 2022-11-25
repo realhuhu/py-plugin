d309c29fcffe58d826c597a07cffe27c837c7341
realhuhu <2661467107@qq.com>
2022-11-25 16:55:01 +0800

    fix

--- index.js
+++ index.js
@@ -20,118 +20,118 @@ setup_server().then(msg => {
   logger.error(err)
 })
 
-// export class PyPlugin extends plugin {
-//   constructor() {
-//     super({
-//       name: 'py-plugin',
-//       dsc: 'py-plugin',
-//       event: 'message',
-//       priority: 5000,
-//       rule: [
-//         {
-//           reg: "#?(n)?py帮助",
-//           fnc: 'py_help'
-//         },
-//         {
-//           reg: "#?(n)?py(下载|卸载|启用|禁用)插件.*",
-//           fnc: 'py_manage'
-//         },
-//       ]
-//     })
-//
-//   }
-//
-//   async py_help(e) {
-//
-//     e.reply("还没写")
-//   }
-//
-//   async py_manage(e) {
-//     if (!e.isMaster) return
-//     let cfg = py_plugin_config
-//     let plugin = e.msg.replace(/#?(n)?py(下载|卸载|启用|禁用)插件/, "")
-//
-//     if (e.msg.indexOf("下载") !== -1) {
-//       e.reply(`下载中:${plugin}`)
-//       let err = await this.poetry_run("pip", "install", plugin)
-//       if (err) {
-//         logger.error(err)
-//         e.reply("出错了，请查看控制台")
-//         return
-//       }
-//       e.reply(`下载完成:${plugin}`)
-//       cfg.plugins.push(plugin)
-//     }
-//
-//     if (e.msg.indexOf("卸载") !== -1) {
-//       if (fs.readdirSync(path.join(py_plugin_path, "plugins")).indexOf(plugin) !== -1) {
-//         e.reply(`无法卸载:${plugin}，只能卸载通过pip安装和指令安装的插件`)
-//         return
-//       }
-//       e.reply(`卸载中:${plugin}`)
-//       let err = await this.poetry_run("pip", "uninstall", plugin)
-//       if (err) {
-//         logger.error(err)
-//         e.reply("出错了，请查看控制台")
-//         return
-//       }
-//       e.reply(`卸载完成:${plugin}`)
-//       let index = cfg.plugins.indexOf(plugin)
-//       if (index !== -1) {
-//         cfg.plugins.splice(index, 1)
-//       }
-//     }
-//
-//     if (e.msg.indexOf("启用") !== -1) {
-//       if (cfg.plugins.indexOf(plugin) !== -1) {
-//         e.reply("该插件已启用!")
-//         return
-//       }
-//       e.reply(`已启用:${plugin}`)
-//       cfg.plugins.push(plugin)
-//     }
-//
-//     if (e.msg.indexOf("禁用") !== -1) {
-//       if (cfg.plugins.indexOf(plugin) === -1) {
-//         e.reply("该插件未启用!")
-//         return
-//       }
-//       e.reply(`已禁用:${plugin}`)
-//       let index = cfg.plugins.indexOf(plugin)
-//       if (index !== -1) {
-//         cfg.plugins.splice(index, 1)
-//       }
-//     }
-//
-//     await this.save_cfg(cfg)
-//     await setup_server()
-//     e.reply(`已重启python服务器`)
-//   }
-//
-//   async poetry_run(...args) {
-//     return new Promise(resolve => {
-//       const cmd = spawn(
-//         "poetry",
-//         ["run", ...args],
-//         {
-//           cwd: global.py_plugin_path,
-//         },
-//       );
-//
-//       cmd.on("exit", () => {
-//         resolve()
-//       })
-//
-//       cmd.on("error", resolve)
-//     })
-//   }
-//
-//   async save_cfg(data) {
-//     return new Promise(resolve => {
-//       let yamlStr = YAML.stringify(data);
-//       fs.writeFile(path.join(py_plugin_path, "config.yaml"), yamlStr, () => {
-//         resolve()
-//       });
-//     })
-//   }
-// }
+export class PyPlugin extends plugin {
+  constructor() {
+    super({
+      name: 'py-plugin',
+      dsc: 'py-plugin',
+      event: 'message',
+      priority: 5000,
+      rule: [
+        {
+          reg: "#?(n)?py帮助",
+          fnc: 'py_help'
+        },
+        {
+          reg: "#?(n)?py(下载|卸载|启用|禁用)插件.*",
+          fnc: 'py_manage'
+        },
+      ]
+    })
+
+  }
+
+  async py_help(e) {
+
+    e.reply("还没写")
+  }
+
+  async py_manage(e) {
+    if (!e.isMaster) return
+    let cfg = py_plugin_config
+    let plugin = e.msg.replace(/#?(n)?py(下载|卸载|启用|禁用)插件/, "")
+
+    if (e.msg.indexOf("下载") !== -1) {
+      e.reply(`下载中:${plugin}`)
+      let err = await this.poetry_run("pip", "install", plugin)
+      if (err) {
+        logger.error(err)
+        e.reply("出错了，请查看控制台")
+        return
+      }
+      e.reply(`下载完成:${plugin}`)
+      cfg.plugins.push(plugin)
+    }
+
+    if (e.msg.indexOf("卸载") !== -1) {
+      if (fs.readdirSync(path.join(py_plugin_path, "plugins")).indexOf(plugin) !== -1) {
+        e.reply(`无法卸载:${plugin}，只能卸载通过pip安装和指令安装的插件`)
+        return
+      }
+      e.reply(`卸载中:${plugin}`)
+      let err = await this.poetry_run("pip", "uninstall", plugin)
+      if (err) {
+        logger.error(err)
+        e.reply("出错了，请查看控制台")
+        return
+      }
+      e.reply(`卸载完成:${plugin}`)
+      let index = cfg.plugins.indexOf(plugin)
+      if (index !== -1) {
+        cfg.plugins.splice(index, 1)
+      }
+    }
+
+    if (e.msg.indexOf("启用") !== -1) {
+      if (cfg.plugins.indexOf(plugin) !== -1) {
+        e.reply("该插件已启用!")
+        return
+      }
+      e.reply(`已启用:${plugin}`)
+      cfg.plugins.push(plugin)
+    }
+
+    if (e.msg.indexOf("禁用") !== -1) {
+      if (cfg.plugins.indexOf(plugin) === -1) {
+        e.reply("该插件未启用!")
+        return
+      }
+      e.reply(`已禁用:${plugin}`)
+      let index = cfg.plugins.indexOf(plugin)
+      if (index !== -1) {
+        cfg.plugins.splice(index, 1)
+      }
+    }
+
+    await this.save_cfg(cfg)
+    await setup_server()
+    e.reply(`已重启python服务器`)
+  }
+
+  async poetry_run(...args) {
+    return new Promise(resolve => {
+      const cmd = spawn(
+        "poetry",
+        ["run", ...args],
+        {
+          cwd: global.py_plugin_path,
+        },
+      );
+
+      cmd.on("exit", () => {
+        resolve()
+      })
+
+      cmd.on("error", resolve)
+    })
+  }
+
+  async save_cfg(data) {
+    return new Promise(resolve => {
+      let yamlStr = YAML.stringify(data);
+      fs.writeFile(path.join(py_plugin_path, "config.yaml"), yamlStr, () => {
+        resolve()
+      });
+    })
+  }
+}
