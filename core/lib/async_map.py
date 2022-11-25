b3985e48baf735e89eac6f83ee94fb22feb0a393
realhuhu <2661467107@qq.com>
2022-11-25 16:47:03 +0800

    断线

--- core/lib/async_map.py
+++ core/lib/async_map.py
@@ -1,6 +1,5 @@
 import asyncio
-from collections import deque
-from typing import Union, Any, Dict, Optional
+from typing import Union, Any, Dict, Optional,Tuple
 
 
 class AsyncMapMCS(type):
@@ -15,43 +14,26 @@ class AsyncMapMCS(type):
 
 class AsyncMap(metaclass=AsyncMapMCS):
     def __init__(self, type):
-        self.type = type
-        self._map = {}
-        self._getters = deque()
+        self.type:str = type
+        self._map:Dict[str,asyncio.Future] = {}
 
     def __str__(self):
         return f"<AsyncMap({self.type}) id={id(self)}>"
 
-    @staticmethod
-    def _wakeup_next(waiters: deque):
-        while waiters:
-            waiter = waiters.popleft()
-            if not waiter.done():
-                waiter.set_result(None)
-                break
-
     async def set(self, key: Union[int, str], value: Any) -> None:
-        self._map[key] = value
-        self._wakeup_next(self._getters)
+        future = self._map.get(key)
+        if future:
+            future.set_result(value)
 
-    async def get(self, key: Union[int, str]) -> Any:
-        while not self._map.get(key):
-            getter = asyncio.Future()
-            self._getters.append(getter)
-            try:
-                await getter
-            except:
-                getter.cancel()
-                try:
-                    self._getters.remove(getter)
-                except ValueError:
-                    pass
-                if self._map.get(key) and not getter.cancelled():
-                    self._wakeup_next(self._getters)
-                raise
-        item = self._map[key]
-        del self._map[key]
-        return item
+    async def get(self, key: Union[int, str], timeout=10) -> Any:
+        future = asyncio.get_event_loop().create_future()
+        self._map[key] = future
+        try:
+            return await asyncio.wait_for(future, timeout)
+        except asyncio.TimeoutError:
+            return None
+        finally:
+            del self._map[key]
 
 
 __all__ = [
