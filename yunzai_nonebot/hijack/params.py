8e39634ecda6dc6cde1d6bec8a04878229f88b03
realhuhu <2661467107@qq.com>
2023-03-22 14:36:46 +0800

    修复haruka_bot

--- yunzai_nonebot/hijack/params.py
+++ yunzai_nonebot/hijack/params.py
@@ -1,4 +1,4 @@
-from typing import Any, get_args
+from typing import Any, get_args, Union
 
 from nonebot import logger
 from nonebot.internal import params
@@ -9,7 +9,10 @@ from .bot import OneBot
 from .event import OneEvent
 
 
-async def bot_solve(self, bot: OneBot, **kwargs: Any) -> Any:
+async def bot_solve(self, bot: Union[OneBot, v11.Bot, v12.Bot], **kwargs: Any) -> Any:
+    if not isinstance(bot, OneBot):
+        return bot
+
     checker = self.extra.get("checker")
     if not checker or get_args(checker.type_) or checker.type_ == v11.bot.Bot:
         return bot.v11
@@ -17,15 +20,21 @@ async def bot_solve(self, bot: OneBot, **kwargs: Any) -> Any:
     return bot.v12
 
 
-async def bot_check(self, bot: OneBot, **kwargs: Any) -> None:
+async def bot_check(self, bot: Union[OneBot, v11.Bot, v12.Bot], **kwargs: Any) -> None:
     if checker := self.extra.get("checker"):
-        types = get_args(checker.type_) or (checker.type_,)
-        if not any(map(lambda x: issubclass(x, v11.bot.Bot) or issubclass(x, v12.bot.Bot), types)):
-            logger.error("py-plugin只支持OneBot V11和OneBot V12的插件")
-            raise SkippedException
+        if not isinstance(bot, OneBot):
+            params.check_field_type(checker, bot)
+        else:
+            types = get_args(checker.type_) or (checker.type_,)
+            if not any(map(lambda x: issubclass(x, v11.bot.Bot) or issubclass(x, v12.bot.Bot), types)):
+                logger.error("py-plugin只支持OneBot V11和OneBot V12的插件")
+                raise SkippedException
 
 
-async def event_solve(self, event: OneEvent, **kwargs: Any) -> Any:
+async def event_solve(self, event: Union[OneEvent, v11.Event, v12.Event], **kwargs: Any) -> Any:
+    if not isinstance(event, OneEvent):
+        return event
+
     checker = self.extra.get("checker")
     if not checker or get_args(checker.type_) or issubclass(checker.type_, v11.event.Event):
         return event.v11
@@ -33,12 +42,15 @@ async def event_solve(self, event: OneEvent, **kwargs: Any) -> Any:
     return event.v12
 
 
-async def event_check(self, bot: OneBot, **kwargs: Any) -> None:
+async def event_check(self, event:  Union[OneEvent, v11.Event, v12.Event], **kwargs: Any) -> None:
     if checker := self.extra.get("checker"):
-        types = get_args(checker.type_) or (checker.type_,)
-        if not any(map(lambda x: issubclass(x, v11.event.Event) or issubclass(x, v12.event.Event), types)):
-            logger.error("py-plugin只支持OneBot V11和OneBot V12的插件")
-            raise SkippedException
+        if not isinstance(event, OneEvent):
+            params.check_field_type(checker, event)
+        else:
+            types = get_args(checker.type_) or (checker.type_,)
+            if not any(map(lambda x: issubclass(x, v11.event.Event) or issubclass(x, v12.event.Event), types)):
+                logger.error("py-plugin只支持OneBot V11和OneBot V12的插件")
+                raise SkippedException
 
 
 def hijack_params():
