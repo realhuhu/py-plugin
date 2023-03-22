fd5cfdac2d4becf57c65fddb01e7a74faa2b4fd6
realhuhu <2661467107@qq.com>
2023-03-22 16:04:10 +0800

    fix message_id

--- yunzai_nonebot/utils/deserializer/v11/utils.py
+++ yunzai_nonebot/utils/deserializer/v11/utils.py
@@ -1,11 +1,19 @@
 import os
 import base64
+from typing import Union
 from urllib.request import url2pathname
 from urllib.parse import urlparse, unquote
+
 from nonebot.adapters.onebot.v11 import Bot
 
 
-def message_id_to_str(message_id: int) -> str:
+def message_id_to_str(message_id: Union[int, str]) -> str:
+    if isinstance(message_id, str) and not message_id.isdigit():
+        return message_id
+
+    if isinstance(message_id, str) and message_id.isdigit():
+        message_id = int(message_id)
+
     return base64.b64encode(bytes.fromhex("{:08x}".format(message_id))).decode("utf-8")
 
 
